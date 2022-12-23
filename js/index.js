4cdb0571a3c606d442782da1655a29cbddc42a63
Jaeseo Bae <jae912@uab.edu>
2022-12-22 21:12:12 -0600

    tube + typewriting JS

--- js/index.js
+++ js/index.js
@@ -1,3 +1,30 @@
+/* Typewriter */
+var i = 0;
+var j = 0;
+var txt = 'Hi,';
+var txt2 = 'I\'m Drew.';
+var speed = 200;
+var speed2 = 200;
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("first").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
+
+function typeWriter2() {
+    if (j < txt2.length) {
+      document.getElementById("second").innerHTML += txt2.charAt(j);
+      j++;
+      setTimeout(typeWriter2, speed2);
+    }
+}
+
+setTimeout(typeWriter, 0);
+setTimeout(typeWriter2, 1800);
+
 /* Power Button */
 const opening = document.getElementById('opening');
 const powerBtn = document.getElementById('powerBtn');
